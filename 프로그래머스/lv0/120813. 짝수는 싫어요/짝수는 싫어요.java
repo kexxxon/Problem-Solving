fbedba0811854f04f901ea9fc467054fd2030cdb
kexxxon <92264867+kexxxon@users.noreply.github.com>
2022-12-23 01:56:36 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -3,9 +3,7 @@ class Solution {
         int[] answer = new int[(n + 1) / 2];
         
         for (int i = 0; i <= n; i++) {
-            if (i % 2 == 0) {
-                continue;
-            } else { 
+            if (i % 2 != 0) {
                 answer[i / 2] = i;
             }
         }
