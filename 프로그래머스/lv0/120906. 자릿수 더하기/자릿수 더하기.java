96a8e053091341a8660a1f9046042b2bf293770e
kexxxon <92264867+kexxxon@users.noreply.github.com>
2023-01-02 20:46:02 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,7 +2,7 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        while(n > 0){
+        while(n > 0) {
             answer += n % 10;
             n /= 10;
         }
