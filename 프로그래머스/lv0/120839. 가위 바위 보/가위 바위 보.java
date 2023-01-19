686b1d2af1cf2c6e524301b5423c8c51978d025e
kexxxon <92264867+kexxxon@users.noreply.github.com>
2023-01-19 23:57:41 +0900

    [level 0] Title: 가위 바위 보, Time: 19.65 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,13 +1,12 @@
 class Solution {
     public String solution(String rsp) {
         String answer = "";
-        
-        String[] arr = rsp.split("");
-        
-        for (int i = 0;i < arr.length; i++) {
-            if (arr[i].equals("2")) answer += "0";
-            else if (arr[i].equals("0")) answer += "5";
-            else answer += "2";
+
+        for (int i = 0; i < rsp.length(); i++) {
+            answer += (rsp.split("")[i].equals("0") ? "5"
+                     : rsp.split("")[i].equals("2") ? "0"
+                     : rsp.split("")[i].equals("5") ? "2"
+                     : "9");
         }
 
         return answer;
